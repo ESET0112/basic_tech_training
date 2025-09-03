219a8e0ea65a7486f620e3fa14657ee3cd47c96c
Nilanjan <nilanjan.chakraborty@esyasoft.com>
2025-09-03 19:09:10 +0530

    added python folder and kept day1 and day2 tasks in that

---
+++ python/day_1/area.py
@@ -0,0 +1,4 @@
+length = 20
+width = 10
+area = length * width
+print("Area of the rectangle:" + str(area) )
---
+++ python/day_2/even_odd.py
@@ -0,0 +1,6 @@
+numbers = [1,2,3,4,5,6]
+for i in numbers:
+    if(i%2==0):
+        print(str(i) + "is even")
+    else:
+        print(str(i) + "is odd")
