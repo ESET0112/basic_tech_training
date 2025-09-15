91e591d493431ab71baf521c7e8f7f1a15606968
Nilanjan <nilanjan.chakraborty@esyasoft.com>
2025-09-15 15:39:42 +0530

    Class,Object,Method,Constructors and Constructor Overloading

---
+++ dotnet/class_exercise/day_2/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace class_demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //student first = new student();
+            student first = new student(10, "abcd", 25, "1234", "abcd@gmail.com");
+            //first.initialize();
+            first.showDisplay();
+            first.name = "Nilanjan";
+            first.showDisplay();
+            
+           
+        }
+    }
+}
---
+++ dotnet/class_exercise/day_2/student.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class_demo
+{
+    internal class student
+    {
+        private int student_id;
+        public string name;
+        int student_age;
+        string contact_no;
+        string emailid;
+
+        //public void initialize()
+        //{
+        //    student_id = 10;
+        //    name = "abcd";
+        //}
+
+        public void showDisplay()
+        {
+            Console.WriteLine("Displaying object");
+            Console.WriteLine(student_id);
+            Console.WriteLine(name);
+            Console.WriteLine(student_age);
+            Console.WriteLine(contact_no);
+            Console.WriteLine(emailid);
+        }
+
+        public student(int id, string std_name, int age, string contact, string email)    //Parameterized Constructor
+        {
+            student_id = id;
+            name = std_name;
+            student_age = age;
+            contact_no = contact;
+            emailid = email;
+        }
+        public student() { }   //Default Constructor
+
+
+    }
+}
