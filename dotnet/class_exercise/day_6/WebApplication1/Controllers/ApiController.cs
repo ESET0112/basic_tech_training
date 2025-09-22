954f5a7fb69f5e31f5b4b6d377b9956b13d6a4e0
Nilanjan <nilanjan.chakraborty@esyasoft.com>
2025-09-22 15:49:26 +0530

    Added Web application files

---
+++ dotnet/class_exercise/day_6/WebApplication1/Controllers/ApiController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApiController : ControllerBase
+    {
+        private List<string> students = new List<string>
+        {
+            "abcd",
+            "sachin",
+            "rahul",
+        };
+
+        private List<Dictionary<string, string>> student_dict = new List<Dictionary<string, string>>
+        {
+            new Dictionary<string, string>{{"id","1"},{"name","Sachin"}},
+            new Dictionary<string, string>{{"id","2"},{"name","Rahul"}},
+        };
+
+        [HttpGet]
+
+        public IActionResult GetAllStudents()
+        {
+            return new JsonResult(student_dict);
+        }
+
+        [HttpGet("{id}")]
+
+        public IActionResult GetStudentById(string id)
+        {
+            var student_details = student_dict.FirstOrDefault(p => p["id"] == id);
+            if (student_details != null)
+            {
+                return new JsonResult(student_details);
+            }
+            return new JsonResult("No matching result found");
+
+        }
+    }
+}
