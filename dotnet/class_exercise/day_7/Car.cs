18bc54013c11731bc831cf4dc3784426c3a80885
Nilanjan <nilanjan.chakraborty@esyasoft.com>
2025-09-24 17:39:08 +0530

    Interfacing

---
+++ dotnet/class_exercise/day_7/Car.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    internal class Car:Interface1
+    {
+        public void drive() {
+            Console.WriteLine("Car is Driving");
+        }
+    }
+}
---
+++ dotnet/class_exercise/day_7/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Interface
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Bike bike = new Bike();
+            bike.drive();
+
+            Car car = new Car();
+            car.drive();
+        }
+    }
+}
